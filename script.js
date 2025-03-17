687ea1e193419e8dca4c279fe7ca3d4917bbd64a
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-03-17 15:56:10 -0400

    Update checkmarker function update change function for board

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const board = (function () {
     let gameboard = [[0,0,0], [0,0,0], [0,0,0]];
     
-    const change = (a, b) => {
-        gameboard[a][b] = 1
+    const change = (a, b, marker) => {
+        if (marker == "X") {
+            gameboard[a][b] = "X"
+        } else {
+            gameboard[a][b] = "O"
+        }
     }
 
     return {gameboard, change};
 })();
 
-console.log(board.gameboard)
-console.log(board.gameboard[2])
-console.log(board.gameboard[2][1])
+function createPlayer (name, num) {
+    const playerNumber = "Player " + num;
+    let marker = "";
+    const checkMarker = (function(num) {
+        if (num == 1) {
+            marker = "X"
+        } else {
+            marker = "O";
+        }
+    })(num);
+    return {name, playerNumber, marker}
+}
 
-board.change(2, 1)
+const me = createPlayer ("Me", "1")
+const dog = createPlayer ("Roma", "2")
+
+console.log(me)
+console.log(dog)
+
+board.change(2, 1, me.marker)
+board.change(2, 2, dog.marker)
 
 console.log(board.gameboard)
 console.log(board.gameboard[2])
