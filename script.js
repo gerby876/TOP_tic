a8a88e0dcbb5cab8cd262151e63257ff221adc8e
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-03-19 16:16:28 -0400

    Organize code into groups

--- script.js
+++ script.js
@@ -71,266 +71,67 @@ const board = (function () {
     };
 
     const newMatch = () => {
-        gameboard=[[1,2,3],[4,5,6],[7,8,9]];
-        t=0;
-        gameend=0;
-        active=1;
-        score1 = 0
-        score2 = 0
-        display.updateScore(score1, score2)
+        newGame();
+        score1 = 0;
+        score2 = 0;
+        display.updateScore(score1, score2);
     };
 
-    return {turn, newGame, newMatch};
+    return {turn, newGame, newMatch, gameboard};
 })();
 
 
 
 const display = (function() {
-    let active = 1;
     let gameend = 0;
+    let active = 1;
     let start = 0;
+    let name1 = ""
+    let name2 = ""
 
+    const players = document.querySelector(".players");
+    const bgame = document.getElementsByClassName("game");
+    const newGame = document.querySelector(".ngame");
+    const newMatch = document.querySelector(".nmatch");
+    const b1 = document.querySelector(".b1");
+    const b2 = document.querySelector(".b2");
+    const b3 = document.querySelector(".b3");
+    const b4 = document.querySelector(".b4");
+    const b5 = document.querySelector(".b5");
+    const b6 = document.querySelector(".b6");
+    const b7 = document.querySelector(".b7");
+    const b8 = document.querySelector(".b8");
+    const b9 = document.querySelector(".b9");;
+    const s1 = document.querySelector("#score1")
+    const s2 = document.querySelector("#score2");
+    const p1 = document.querySelector("#p1");
+    const p2 = document.querySelector("#p2");
+    const player1 = document.querySelector("#player1");
+    const player2 = document.querySelector("#player2");
+    const vs = document.querySelector("#vs");
+    
     const createPlayer = () => {
-        if (document.getElementById("p1") !== "") {
-            name1 = document.getElementById("p1").value;
+        if (p1 !== "") {
+            name1 = p1.value;
         };
-        if (document.getElementById("p2") !== "") {
-            name2 = document.getElementById("p2").value;
+        if (p2 !== "") {
+             name2 = p2.value;
         };
-        return {name1, name2};
     };
 
     const updateDisplay = () => {
         if (name1 !== "") {
-            document.getElementById("player1").textContent = name1;
-            document.getElementById("p1").value = "";
+            player1.textContent = name1;
+            p1.value = "";
         }
         if (name2 !== "") {
-            document.getElementById("player2").textContent = name2;
-            document.getElementById("p2").value = "";
+            player2.textContent = name2;
+            p2.value = "";
         }
 
     };
 
-    const players = document.querySelector(".players");
-
-    players.addEventListener("click", () => {
-        createPlayer();
-        updateDisplay();
-        for (let i=0; i<bgame.length; i++) {
-            bgame[i].textContent = ""
-            bgame[i].removeAttribute("id");
-        };
-        document.getElementById("vs").textContent = "VS";
-        document.getElementById("vs").style.fontSize = "5em";
-        document.getElementById("vs").style.backgroundColor = "";
-        document.getElementById("score1").style.fontSize = "30em";
-        document.getElementById("score2").style.fontSize = "30em";
-        board.newMatch();
-        active = 1;
-        gameend = 0;
-        start = 1;
-    });
-
-    const bgame = document.getElementsByClassName("game");
-
-    const newGame = document.querySelector(".ngame");
-    newGame.addEventListener("click", () => {
-        board.newGame();
-        for (let i=0; i<bgame.length; i++) {
-            bgame[i].textContent = "";
-            bgame[i].removeAttribute("id");
-        };
-        document.getElementById("vs").textContent = "VS";
-        document.getElementById("vs").style.fontSize = "5em";
-        document.getElementById("vs").style.backgroundColor = "";
-        active = 1;
-        gameend = 0;
-        start = 1;
-    });
-
-    const newMatch = document.querySelector(".nmatch");
-    newMatch.addEventListener("click", () => {
-        board.newMatch();
-        for (let i=0; i<bgame.length; i++) {
-            bgame[i].textContent = "";
-            bgame[i].removeAttribute("id");
-        };
-        document.getElementById("vs").textContent = "VS"
-        document.getElementById("vs").style.fontSize = "5em"
-        document.getElementById("vs").style.backgroundColor = "";
-        document.getElementById("score1").style.fontSize = "30em";
-        document.getElementById("score2").style.fontSize = "30em";
-        active = 1;
-        gameend = 0;
-        start = 1;
-    })
-
-    const b1 = document.querySelector(".b1");
-    b1.addEventListener("click", () => {
-        if (b1.textContent == "X" || b1.textContent == "O" || gameend == 1 || start == 0)
-        {
-            return
-        };
-        if (active == 1) {
-            b1.textContent="X"
-            active = 2
-            b1.id = "player1"
-        } else {
-            b1.textContent="O"
-            active = 1
-            b1.id = "player2"
-        };
-        board.turn(0,0);
-    });
-    
-    const b2 = document.querySelector(".b2");
-    b2.addEventListener("click", () => {
-        if (b2.textContent == "X" || b2.textContent == "O" || gameend == 1 || start == 0)
-            {
-                return
-            };
-        if (active == 1) {
-            b2.textContent="X"
-            active = 2
-            b2.id = "player1"
-        } else {
-            b2.textContent="O"
-            active = 1
-            b2.id = "player2"
-        };
-        board.turn(0,1);
-    });
-
-    const b3 = document.querySelector(".b3");
-    b3.addEventListener("click", () => {
-        if (b3.textContent == "X" || b3.textContent == "O" || gameend == 1 || start == 0)
-            {
-                return
-            };
-        if (active == 1) {
-            b3.textContent="X"
-            active = 2
-            b3.id = "player1"
-        } else {
-            b3.textContent="O"
-            active = 1
-            b3.id = "player2"
-        };
-        board.turn(0,2);
-    });
-
-    const b4 = document.querySelector(".b4");
-    b4.addEventListener("click", () => {
-        if (b4.textContent == "X" || b4.textContent == "O" || gameend == 1 || start == 0) 
-            {
-                return
-            };
-        if (active == 1) {
-            b4.textContent="X"
-            active = 2
-            b4.id = "player1"
-        } else {
-            b4.textContent="O"
-            active = 1
-            b4.id = "player2"
-        };
-        board.turn(1,0);
-    });
-
-    const b5 = document.querySelector(".b5");
-    b5.addEventListener("click", () => {
-        if (b5.textContent == "X" || b5.textContent == "O" || gameend == 1 || start == 0)
-            {
-                return
-            };
-        if (active == 1) {
-            b5.textContent="X"
-            active = 2
-            b5.id = "player1"
-        } else {
-            b5.textContent="O"
-            active = 1
-            b5.id = "player2"
-        };
-        board.turn(1,1);
-    });
-
-    const b6 = document.querySelector(".b6");
-    b6.addEventListener("click", () => {
-        if (b6.textContent == "X" || b6.textContent == "O" || gameend == 1 || start == 0)
-            {
-                return
-            };
-        if (active == 1) {
-            b6.textContent="X"
-            active = 2
-            b6.id = "player1"
-        } else {
-            b6.textContent="O"
-            active = 1
-            b6.id = "player2"
-        };
-        board.turn(1,2);
-    });
-
-    const b7 = document.querySelector(".b7");
-    b7.addEventListener("click", () => {
-        if (b7.textContent == "X" || b7.textContent == "O" || gameend == 1 || start == 0)
-            {
-                return
-            };
-        if (active == 1) {
-            b7.textContent="X"
-            active = 2
-            b7.id = "player1"
-        } else {
-            b7.textContent="O"
-            active = 1
-            b7.id = "player2"
-        };
-        board.turn(2,0);
-    });
-
-    const b8 = document.querySelector(".b8");
-    b8.addEventListener("click", () => {
-        if (b8.textContent == "X" || b8.textContent == "O" || gameend == 1 || start == 0)
-            {
-                return
-            };
-        if (active == 1) {
-            b8.textContent="X"
-            active = 2
-            b8.id = "player1"
-        } else {
-            b8.textContent="O"
-            active = 1
-            b8.id = "player2"
-        };
-        board.turn(2,1);
-    });
-
-    const b9 = document.querySelector(".b9");
-    b9.addEventListener("click", () => {
-        if (b9.textContent == "X" || b9.textContent == "O" || gameend == 1 || start == 0)
-            {
-                return
-            };
-        if (active == 1) {
-            b9.textContent="X"
-            active = 2
-            b9.id = "player1"
-        } else {
-            b9.textContent="O"
-            active = 1
-            b9.id = "player2"
-        };
-        board.turn(2,2);
-    });
-
     const updateScore = (score1, score2) => {
-        const s1 = document.getElementById("score1")
-        const s2 = document.getElementById("score2")
         s1.textContent = score1;
         s2.textContent = score2;
         if (score1 > 9) {
@@ -342,7 +143,7 @@ const display = (function() {
     };
 
     const winLose = (active, name1, name2) => {
-        const vs = document.querySelector("#vs");
+        
         if (active == 1) {
             vs.textContent = name1 + " WINS!"
             vs.style.backgroundColor =" #D3AE36";
@@ -355,10 +156,150 @@ const display = (function() {
     };
 
     const draw = () => {
-        const vs = document.querySelector("#vs");
         vs.textContent = "DRAW"
     }
 
+    const clearBoard = () => {
+        for (let i=0; i<bgame.length; i++) {
+            bgame[i].textContent = ""
+            bgame[i].removeAttribute("id");
+        };
+    }
+
+    const setVs = () => {
+        vs.textContent = "VS";
+        vs.style.fontSize = "5em";
+        vs.style.backgroundColor = "";
+    }
+
+    const scoreSize = () => {
+        s1.style.fontSize = "30em";
+        s2.style.fontSize = "30em";
+    }
+
+    const gameState = () => {
+        active = 1;
+        gameend = 0;
+        start = 1;
+    }
+
+    const placeMarker = (button) => {
+        if (active == 1) {
+            button.textContent="X"
+            active = 2
+            button.id = "player1"
+        } else {
+            button.textContent="O"
+            active = 1
+            button.id = "player2"
+        };
+    }
+
+    players.addEventListener("click", () => {
+        createPlayer();
+        updateDisplay();
+        clearBoard();
+        setVs();
+        scoreSize();
+        board.newMatch();
+        gameState()
+    });
+
+    newGame.addEventListener("click", () => {
+        board.newGame();
+        clearBoard();
+        setVs();
+        gameState()
+    });
+
+    newMatch.addEventListener("click", () => {
+        board.newMatch();
+        clearBoard();
+        setVs();
+        scoreSize();
+        gameState()
+    })
+
+    b1.addEventListener("click", () => {
+        if (b1.textContent == "X" || b1.textContent == "O" || gameend == 1 || start == 0)
+        {
+            return
+        };
+        placeMarker(b1);
+        board.turn(0,0);
+    });
+    
+    b2.addEventListener("click", () => {
+        if (b2.textContent == "X" || b2.textContent == "O" || gameend == 1 || start == 0)
+            {
+                return
+            };
+       placeMarker(b2);
+        board.turn(0,1);
+    });
+
+    b3.addEventListener("click", () => {
+        if (b3.textContent == "X" || b3.textContent == "O" || gameend == 1 || start == 0)
+            {
+                return
+            };
+        placeMarker(b3);
+        board.turn(0,2);
+    });
+
+    b4.addEventListener("click", () => {
+        if (b4.textContent == "X" || b4.textContent == "O" || gameend == 1 || start == 0) 
+            {
+                return
+            };
+        placeMarker(b4);
+        board.turn(1,0);
+    });
+
+    b5.addEventListener("click", () => {
+        if (b5.textContent == "X" || b5.textContent == "O" || gameend == 1 || start == 0)
+            {
+                return
+            };
+        placeMarker(b5);
+        board.turn(1,1);
+    });
+
+    b6.addEventListener("click", () => {
+        if (b6.textContent == "X" || b6.textContent == "O" || gameend == 1 || start == 0)
+            {
+                return
+            };
+        placeMarker(b6);
+        board.turn(1,2);
+    });
+
+    b7.addEventListener("click", () => {
+        if (b7.textContent == "X" || b7.textContent == "O" || gameend == 1 || start == 0)
+            {
+                return
+            };
+        placeMarker(b7);
+        board.turn(2,0);
+    });
+
+    b8.addEventListener("click", () => {
+        if (b8.textContent == "X" || b8.textContent == "O" || gameend == 1 || start == 0)
+            {
+                return
+            };
+        placeMarker(b8);
+        board.turn(2,1);
+    });
+
+    b9.addEventListener("click", () => {
+        if (b9.textContent == "X" || b9.textContent == "O" || gameend == 1 || start == 0)
+            {
+                return
+            };
+        placeMarker(b9);
+        board.turn(2,2);
+    });
+
     return {updateScore, winLose, draw}
 })();
-
